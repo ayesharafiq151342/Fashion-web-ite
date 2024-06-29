c9eb9f0ccb12d982327ec2e2e612919f22776af5
Ayesha Rafiq <ayesharafiq151342@gmail.com>
2024-06-30 00:46:39 +0500

    ash

---
+++ index.js
@@ -0,0 +1,17 @@
+// var data = document.all;
+//check heder 
+var data = document.images;
+var data = document.URL;
+var data = document.domain;
+
+//get id
+var data = document.getElementById("button-2");
+//class name
+//classname use in mny time in one pge so elements ay gy not element 
+//but id my elements ay ga
+ var data = document.getElementsByClassName("image-div")[0]
+
+//by tag name  
+var data = document.getElementsByTagName("ul")[4]
+
+console.log(data)
